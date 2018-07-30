a478ba75e776ab496ce0543a09a53041019bda75
gore <rjhgore@gmail.com>
2018-07-30 10:58:16 -0700

    MONDAY
    qurrent version of my code

--- infile/example.Rmd.R
+++ infile/example.Rmd.R
@@ -2,8 +2,15 @@
 dir <- c("/Users/cigom/Documents/GitHub/July_2018_bioinfo/infile/")
 setwd(dir)
 
-x <- read.table("diffExpr.P1e-2_C1.matrix")
+x <- read.table(paste0(dir,"diffExpr.P1e-2_C1.matrix2"))
 str(x)
+
+load("/Users/cigom/Documents//time0/edgeR.11759.dir/diffExpr.P1e-2_C1.matrix.RData")
+
+tmp <- new.env()
+load(file="/Users/cigom/Documents//time0/edgeR.11759.dir/diffExpr.P1e-2_C1.matrix.RData", env=tmp)
+# ls(env=tmp)
+test <- tmp$data
 
 
 library(ggpubr)
@@ -13,7 +20,7 @@ library(ggplot2)
 
 
 # plot heatmap of diffExp genes
-superheat(x,
+superheat(data,
           # retain original order of rows/cols
           pretty.order.rows = TRUE,
           pretty.order.cols = TRUE,
@@ -37,6 +44,36 @@ ggpubr::ggbarplot(x, x="Gene",
 
 # plot annotation
 library(trinotateR)
+y <- read_trinotate("Trinotate.xls")
+summary_trinotate(y)
+
+
+y1 <- split_pfam(y)
+head(y1,3)[,c(2,4:7)]
+
+y2 <- summary_pfam(x1)
+head(y2[order(-y2$transcripts),])
+
+y2 <- summary_pfam(y1)
+head(y2[order(-y2$transcripts),])
+attr(y2, "count")
+
+
+library(DT)
+z <- data.frame(y2)
+z$pfam <- paste0('<a href="http://pfam.xfam.org/family/', z$pfam, '">', z$pfam, '</a>')
+datatable(z , escape=1, options = list( pageLength = 10 ) )
+
+
+# melting files
+
+x <- melt(z, id.vars = c("Temperature", "Condition"),
+          #measure.vars = c("Hif1α", "Hsp70", "Hsp90", "GPx", "cMnSOD", "Na-K ATPasa"),
+          value.name= "expression", variable_name = "Name")
+
+colnames(x)[c(3:4)] <- c("Gene", "Expression")
+
+
 
 
 
